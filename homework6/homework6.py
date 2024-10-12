5b427d7b29941c397f921dc36c7ef93b268b3db9
kiitrr <renatuz10@gmail.com>
2024-10-12 12:28:14 +0500

    Update homework6.py

--- homework6/homework6.py
+++ homework6/homework6.py
@@ -5,3 +5,9 @@ while True:
     user_input = input("Введите число (или 'end' для завершения): ")
     if user_input.lower() == 'end':
         break
+    try:
+        number = int(user_input)
+        numbers.append(number)  
+    except ValueError:
+        print("Ошибка: введите корректное целое число.")
+
