95728c9409cd40a68f279a6489f8faed34f9f948
kiitrr <renatuz10@gmail.com>
2024-10-12 12:16:23 +0500

    Update and rename homework.py to homework5.py

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -21,3 +21,12 @@ def find_greater_than_previous(numbers):
     def is_greater_by_one(current, previous):
         return current == previous + 1
 
+    
+    greater_than_previous = [numbers[i] for i in range(1, len(numbers)) if is_greater_by_one(numbers[i], numbers[i - 1])]
+    return greater_than_previous
+numbers = input_numbers()
+if numbers:  
+    result = find_greater_than_previous(numbers)
+    print("Элементы, которые больше предыдущего элемента на 1:", result)
+else:
+    print("Список чисел пуст.")
