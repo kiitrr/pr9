631cf4a6711545151e68af3ec17a3d9515125f17
kiitrr <renatuz10@gmail.com>
2024-10-12 13:07:00 +0500

    Update homework9.py

--- homework9/homework9.py
+++ homework9/homework9.py
@@ -2,6 +2,14 @@ import re
 
 def parse_email(email):
     """Разбирает email на имя пользователя и доменное имя."""
-    # Регулярное выражение для проверки и разбора email
+    
     pattern = r'^(?P<username>[a-zA-Z][\w.-]*)@(?P<domain>[\w.-]+\.\w+)$'
     match = re.match(pattern, email)
+
+    if match:
+        username = match.group('username')
+        domain = match.group('domain')
+        return username, domain
+    else:
+        return None, None
+
