c39cf872c894e07c34bfc2fdae4be4afdaa07a1d
kiitrr <renatuz10@gmail.com>
2024-10-12 11:48:13 +0500

    Update and rename homework.py to homework2.py

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -8,3 +8,10 @@ def get_squares(a, b):
     start = min(a, b)
     end = max(a, b)
     
+
+    squares = [i**2 for i in range(start, end + 1)]
+    return squares
+
+try:
+    a = int(input("Введите первое целое число (a): "))
+    b = int(input("Введите второе целое число (b): "))    
